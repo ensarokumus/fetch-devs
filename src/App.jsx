794a1cacdbf5a50653c20f4c84763572c54e6c16
ensaro <41113027+ensarokumus@users.noreply.github.com>
2024-05-11 18:42:07 +1000

    feat: adds HomeCards and Card components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import Hero from "./components/Hero";
-import NavBar from "./components/NavBar";
+import Hero from "./components/atoms/Hero";
+import NavBar from "./components/atoms/NavBar";
+import HomeCards from "./components/molecules/HomeCards";
 
 const App = () => {
   return (
     <>
       <NavBar />
-      <Hero title="" subtitle="" />
+      <Hero />
+      <HomeCards />
     </>
   );
 };
--- src/components/atoms/NavBar.jsx
+++ src/components/atoms/NavBar.jsx
@@ -1,4 +1,4 @@
-import logo from "../assets/images/logo-fetchDEVS.png";
+import logo from "../../assets/images/logo-fetchDEVS.png";
 
 const NavBar = () => {
   return (
