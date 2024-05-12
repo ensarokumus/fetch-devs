76953644205c79799fb9c634b4e92d8612b23bbc
ensaro <41113027+ensarokumus@users.noreply.github.com>
2024-05-12 23:48:42 +1000

    feat: adds state to descriptions on JobListing

--- src/components/atoms/JobListing.jsx
+++ src/components/atoms/JobListing.jsx
@@ -1,5 +1,15 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
+
 const JobListing = ({ job }) => {
+  const [showFullDescription, setShowFullDescription] = useState(false);
+
+  let description = job.description;
+
+  if (!showFullDescription) {
+    description = description.substring(0, 90) + "...";
+  }
+
   return (
     <div key={job.id} className="bg-white rounded-xl shadow-md relative">
       <div className="p-4">
@@ -8,13 +18,20 @@ const JobListing = ({ job }) => {
           <h3 className="text-xl font-bold">{job.title}</h3>
         </div>
 
-        <div className="mb-5">{job.description}</div>
+        <div className="mb-1">{description}</div>
+
+        <button
+          onClick={() => setShowFullDescription((prevState) => !prevState)}
+          className="text-teal-500 mb-4 hover:text-teal-700"
+        >
+          {showFullDescription ? "Less" : "More"}
+        </button>
 
         <h3 className="text-teal-600 mb-2">{job.salary} / Year</h3>
 
         <div className="border border-gray-100 mb-5"></div>
 
-        <div className="flex flex-col lg:flex-row justify-between mb-4">
+        <div className="flex flex-col lg:flex-row justify-between">
           <div className="text-gray-700 mb-3">
             <i className="fa-solid fa-location-dot text-lg"></i>
             {job.location}
