3170a469d521df25cccc59d750d2025dbfaeded1
ensaro <41113027+ensarokumus@users.noreply.github.com>
2024-05-13 11:53:06 +1000

    feat: adds JobsPage

--- src/pages/JobsPage.jsx
+++ src/pages/JobsPage.jsx
@@ -1,5 +1,11 @@
+import JobListings from "../components/molecules/JobListings";
+
 const JobsPage = () => {
-  return <div>JobsPage</div>;
+  return (
+    <section className="bg-cyan-50 px-4 py-6">
+      <JobListings />
+    </section>
+  );
 };
 
 export default JobsPage;
