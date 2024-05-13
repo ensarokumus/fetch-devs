d9f2f59033e65f0dd1ddd79fadb3714ce9760698
ensaro <41113027+ensarokumus@users.noreply.github.com>
2024-05-13 11:47:21 +1000

    feat: adds NavLink for NavBar

--- src/components/atoms/NavBar.jsx
+++ src/components/atoms/NavBar.jsx
@@ -1,13 +1,17 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import logo from "../../assets/images/logo-fetchDEVS.png";
 
 const NavBar = () => {
+  const LinkClass = ({ isActive }) =>
+    isActive
+      ? "text-cyan-950 bg-teal-500 hover:bg-teal-700 hover:text-white rounded-md px-3 py-2"
+      : "text-white hover:bg-teal-700 hover:text-white rounded-md px-3 py-2";
   return (
     <nav className="bg-cyan-900 border-b border-cyan-600">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="flex h-20 items-center justify-between">
           <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
-            <Link className="flex flex-shrink-0 items-center mr-4" to="/">
+            <NavLink className="flex flex-shrink-0 items-center mr-4" to="/">
               <img className="h-10 w-auto" src={logo} alt="fetchDEVS" />
               <span className="hidden md:block text-white text-2xl font-bold ml-2">
                 fetch
@@ -15,27 +19,18 @@ const NavBar = () => {
               <span className="hidden md:block text-teal-500 text-2xl font-bold">
                 DEVS
               </span>
-            </Link>
+            </NavLink>
             <div className="md:ml-auto">
               <div className="flex space-x-2">
-                <Link
-                  to="/"
-                  className="text-cyan-950 bg-teal-500 hover:bg-teal-700 hover:text-white rounded-md px-3 py-2"
-                >
+                <NavLink to="/" className={LinkClass}>
                   Home
-                </Link>
-                <Link
-                  to="/jobs"
-                  className="text-white hover:bg-teal-700 hover:text-white rounded-md px-3 py-2"
-                >
+                </NavLink>
+                <NavLink to="/jobs" className={LinkClass}>
                   Jobs
-                </Link>
-                <Link
-                  to="/add-job.html"
-                  className="text-white hover:bg-teal-700 hover:text-white rounded-md px-3 py-2"
-                >
+                </NavLink>
+                <NavLink to="/add-job.html" className={LinkClass}>
                   Add Job
-                </Link>
+                </NavLink>
               </div>
             </div>
           </div>
